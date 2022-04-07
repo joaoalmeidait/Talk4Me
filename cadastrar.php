1f7145aecb37bb45dd34145c254cc4686f47dbe5
João Almeida <joaoalmeidapedrosa@gmail.com>
2022-04-06 21:48:27 -0300

    ajuste branch main

--- cadastrar.php
+++ cadastrar.php
@@ -15,7 +15,7 @@ if (isset($_POST['email']) && !empty($_POST['email'])){
 }else {
     header("Location: cadastro.php");
 }if (mysqli_insert_id($conexao)) {
-    header("Location: carrinho.php");
+    header("Location: Interpretes.php");
     # code...
 
 }
