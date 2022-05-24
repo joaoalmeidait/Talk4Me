00e0ec4035ff114a2abe7b1498963b0a6ea20b93
João Almeida <joaoalemidapedrosa@gmail.com>
2022-05-24 14:56:36 -0300

    ajuste para registrar atendimento

--- registrar_atendimento.php
+++ registrar_atendimento.php
@@ -3,18 +3,18 @@ session_start();
 include_once ("conexao1.php");
 $email_interprete= filter_input(INPUT_POST, 'email_interprete', FILTER_SANITIZE_STRING);
 $data=filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
-$email_usuario= filter_input(INPUT_POST, 'email_usuario', FILTER_SANITIZE_EMAIL);
+$email_usuario= filter_input(INPUT_POST, 'email_usuario', FILTER_SANITIZE_STRING);
 $duracao=filter_input(INPUT_POST, 'duracao', FILTER_SANITIZE_STRING);
 $descricao=filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_STRING);
 
-$id_usuario="SELECT 'id' FROM `usuarios` where 'email' = '$email_usuario'";
+$id_usuario="SELECT id FROM `usuarios` WHERE `email` = '$email_usuario'";
 $id_usuario=mysqli_query($conexao, $id_usuario);
 $dado_u = $id_usuario->fetch_array();
-    $id_u = $dado_u['id'];
+$id_u = $dado_u['id'];
 
 
 
-$id_worker="SELECT 'id' FROM `interpretes` where 'email' = '$email_interprete'";
+$id_worker="SELECT id FROM `interpretes` WHERE `email` = '$email_interprete'";
 $id_worker=mysqli_query($conexao, $id_worker);
 $dado_w = $id_worker->fetch_array();
 $id_w = $dado_w['id'];
@@ -29,9 +29,9 @@ if (isset($_POST['email_usuario']) && !empty($_POST['email_usuario'])){
 
     
 }else {
-    header("Location: atendimento_registrado.html");
-}if (mysqli_insert_id($conexao)) {
     header("Location: Registro_atendimento.html");
+}if (mysqli_insert_id($conexao)) {
+    header("Location: atendimento_registrado.html");
     # code..
 }
 
